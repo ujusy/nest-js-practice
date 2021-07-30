179bbbac3e9ea7fabc819973c55cc76d85da66a2
ujusy <lsyeon1997@ajou.ac.kr>
2021-07-30 17:08:11 +0900

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('UJUSY API')
+    .setDescription('api 개발 문서')
+    .setVersion('1.0')
+    .addTag('uju')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
