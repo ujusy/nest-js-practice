09068556f2bff144b309444dcf7e388adcc02e22
ujusy <lsyeon1997@ajou.ac.kr>
2021-08-02 22:36:43 +0900

    create user dto

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,16 @@
-export class CreateUserDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateUserDto {
+  @IsEmail()
+  @ApiProperty({
+    example: 'lsyeon1997@naver.com',
+    description: '이메일',
+  })
+  public email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({})
+  public name: string;
+}
